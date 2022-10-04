0fe76d635ed4b5ac7a7d7a0f6b995ab02623d67a
gumodi10@gmail.com <gumodi10@gmail.com>
2022-10-04 16:42:58 -0500

    Add Add component

--- book-app-manager/src/components/AddBook.js
+++ book-app-manager/src/components/AddBook.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import BookForm from './BookForm';
 
-const AddBook = () => {
+const AddBook = ({ history, books, setBooks }) => {
   const handleOnSubmit = (book) => {
-    console.log(book);
+    setBooks([book, ...books]);
   };
 
   return (
