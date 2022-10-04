63014c024948ffb2a80b3bb68f66f231d92ddce2
gumodi10@gmail.com <gumodi10@gmail.com>
2022-10-04 16:40:03 -0500

    Add useLocalStorage

--- book-app-manager/src/router/AppRouter.js
+++ book-app-manager/src/router/AppRouter.js
@@ -3,8 +3,14 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Header from '../components/Header';
 import AddBook from '../components/AddBook';
 import BooksList from '../components/BooksList';
+import useLocalStorage from '../hooks/useLocalStorage';
+
 
 const AppRouter = () => {
+
+    const AppRouter = () => {
+        const [books, setBooks] = useLocalStorage('books', []);
+
   return (
     <BrowserRouter>
       <div>
@@ -13,11 +19,17 @@ const AppRouter = () => {
           <Switch>
             <Route component={BooksList} path="/" exact={true} />
             <Route component={AddBook} path="/add" />
+            <Route
+                render = {(props) => (
+                <AddBook {...props} books={books} setBooks={setBooks} /> 
+                )}
+                path = "/add"
+            />
           </Switch>
         </div>
       </div>
     </BrowserRouter>
   );
 };
-
+}
 export default AppRouter;
